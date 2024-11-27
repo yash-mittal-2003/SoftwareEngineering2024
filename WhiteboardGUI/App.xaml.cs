1683ab3a9d396cb5ecbf8a4fdcfe17e71908cf42
yash-mittal-2003 <yashmittal2003@gmail.com>
2024-11-27 20:42:13 +0530

    Fixed canvas clipping

--- WhiteboardGUI/App.xaml.cs
+++ WhiteboardGUI/App.xaml.cs
@@ -2,13 +2,11 @@
 using System.Data;
 using System.Windows;
 
-namespace WhiteboardGUI
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace WhiteboardGUI;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
 }
